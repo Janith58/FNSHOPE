18f5670bccd116da50a621af47900b14b7e375e8
Janith58 <janithwannigama@gamail.com>
2024-09-13 17:28:05 +0530

    create header component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SignIn = () => {
   return (
     <div>
-      
+      <h1>this is signin page</h1>
     </div>
   )
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SignUp = () => {
   return (
     <div>
-      
+      <h1>this is signup page</h1>
     </div>
   )
 }
