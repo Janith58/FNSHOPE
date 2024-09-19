d4158a9dbf9f95ecf9e0837a58d60cb957cdd4ae
Janith58 <janithwannigama@gamail.com>
2024-09-19 07:37:37 +0530

    create midlware

--- backend/controllers/authControler.js
+++ backend/controllers/authControler.js
@@ -1,7 +1,7 @@
 import User from "../models/userMode.js";
 import bcryptjs from 'bcryptjs';
 
-export const singup = async (req,res)=>{
+export const singup = async (req,res,next)=>{
     const {username,email,password} = req.body;
     const hashedPassword = await bcryptjs.hash(password,10);
     const user = new User({username,email,password:hashedPassword});
@@ -9,6 +9,6 @@ export const singup = async (req,res)=>{
         await user.save();
             res.status(201).json({message:"User created successfully"});
         }catch(err){
-            res.status(400).json({message:err.message});
+            next(err)
         }
 }
