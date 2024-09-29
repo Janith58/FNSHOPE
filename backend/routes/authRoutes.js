b029c7d5614ecc3d0219d4d5b2f50019c6e30531
Janith58 <janithwannigama@gamail.com>
2024-09-29 21:41:07 +0530

    create sign in api route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { singup } from '../controllers/authControler.js';
+import { singin } from '../controllers/authControler.js';
 
 const router =express.Router();
 
 router.post("/signup",singup);
+router.post("/signin",singin)
 
 export default router
